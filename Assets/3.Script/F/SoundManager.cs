5f865ec2de3b6fe831284ae31f8f491acfa4c3fe
kmj30 <znls2456@gmail.com>
2026-01-12 16:22:30 +0900

    사운드매니저 스크립트 및 파티클 추가

---
+++ Assets/3.Script/F/SoundManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SoundData
+{
+    public string name;
+    public AudioClip clip;
+}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+
+    
+
+    [Header("오디오 소스 설정")]
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    [Header("효과음 클립 리스트")]
+    public List<SoundData> sfxClips;
+
+    void Awake()
+    {
+        if (instance == null) 
+        {
+            instance = this; 
+            DontDestroyOnLoad(gameObject); 
+        }
+        else 
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+    // 효과음 재생 함수
+    public void PlaySFX(string clipName)
+    {
+        SoundData data = sfxClips.Find(x => x.name == clipName);
+        if (data != null)
+        {
+           
+            sfxSource.PlayOneShot(data.clip);
+        }
+    }
+
+    // BGM 재생 함수
+    public void PlayBGM(AudioClip clip)
+    {
+        bgmSource.clip = clip;
+        bgmSource.loop = true;
+        bgmSource.Play();
+    }
+}
+
+
+
