52733f6ef0a60d8abfc7b58f7c1b6eeee01511f1
RiverPark99 <phs201007@gmail.com>
2026-01-12 13:58:41 +0900

    daedzone

--- Assets/3.Script/A/DeadZone.cs
+++ Assets/3.Script/A/DeadZone.cs
@@ -2,34 +2,21 @@ using UnityEngine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
 using Mirror;
 
 public class DeadZone : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // 플레이어 태그가 달린 물체만 감지
+        if (!NetworkServer.active) return;
+
         if (other.CompareTag("Player"))
         {
-            // 2. 닿은 오브젝트(other)로부터 위(Parent)로 올라가며 스크립트 탐색
-            var respawnController = other.GetComponentInParent<PlayerRespawnController>();
+            PlayerRespawn respawnController = other.GetComponent<PlayerRespawn>();
 
             if (respawnController != null)
             {
-                // 로컬 테스트 모드라면 즉시 리스폰
-                if (respawnController.isLocalTestMode)
-                {
-                    respawnController.RequestRespawn();
-                }
-                // 멀티플레이라면 서버인 경우에만 처리 (중복 방지)
-#if UNITY_SERVER || UNITY_EDITOR
-                else if (NetworkServer.active)
-                {
-                    // 서버 사이드 로직 실행 (DB 갱신 등)
-                    respawnController.OnFellInDeadZone();
-                }
-#endif
+                respawnController.OnFellInDeadZone();
             }
         }
     }
---
+++ Assets/3.Script/B/PlayerRespawn.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+
+public class PlayerRespawn : NetworkBehaviour //민섭님 스크립트 참조
+{
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private Rigidbody rb;
+
+    public override void OnStartLocalPlayer()
+    {
+        transform.TryGetComponent(out rb); //player한테 넣어주세요
+
+        // 1. 소환되자마자 현재 위치를 리스폰 지점으로 저장
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+
+        Debug.Log($"Respawn_Position: {initialPosition}");
+
+        // ---------------------------------------------------------
+        // player nickname?
+        // ---------------------------------------------------------
+    }
+
+    // 서버 사이드에서 점수 처리를 위한 함수 (DeadZone에서 호출)
+    [Server]
+    public void OnFellInDeadZone()
+    {
+        // ---------------------------------------------------------
+        // 체력이 달게 합니다
+        // ---------------------------------------------------------
+
+        TargetRpcRespawn(connectionToClient);
+
+        UIupdateRPC();
+    }
+
+    [TargetRpc]//타겟으로 돌리고
+    void TargetRpcRespawn(NetworkConnection thisconnection)
+    {
+        ExecuteRespawn();
+    }
+
+    //UI전용 ClientRPC사용
+
+    [ClientRpc]
+    private void UIupdateRPC()
+    {
+        Debug.Log("UIupdateRPC");
+        //UIUpdate
+    }
+
+    private void ExecuteRespawn()
+    {
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+
+        Debug.Log("Respawn Complete");
+    }
+}
