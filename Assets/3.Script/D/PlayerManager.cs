20483e5441422d2fbe8b1589c6f274742f4a917e
Jeong Minchan <minchan3465@gmail.com>
2026-01-13 20:18:41 +0900

    플레이어 순번에 따른 스폰

--- Assets/3.Script/D/PlayerManager.cs
+++ Assets/3.Script/D/PlayerManager.cs
@@ -8,26 +8,34 @@ public class PlayerManager : NetworkBehaviour {
     [SyncVar(hook = nameof(OnIndexChanged))]
     public int playernumber = -1;
 
-    // 클라이언트에서 순번이 정해졌을 때 실행될 로직 (예: UI 업데이트 등)
-        //Debug.Log($"내 플레이어 순번이 {newIndex}로 설정되었습니다.");
+    public PlayerRespawn playerRespawn;
 
-    // 서버에서 이 객체가 생성(Spawn)될 때 실행됨
-    // GameManager에게 이 플레이어를 등록해달라고 요청
-    // 근데 내 플레이어가 아니면 꺼지쇼
-    public override void OnStartServer() {
+	// 서버에서 이 객체가 생성(Spawn)될 때 실행됨
+	// GameManager에게 이 플레이어를 등록해달라고 요청
+	// 근데 내 플레이어가 아니면 꺼지쇼
+
+	private void Awake() {
+        TryGetComponent(out playerRespawn);
+    }
+
+	public override void OnStartServer() {
         base.OnStartServer();
         if (GameManager.Instance != null) {
             GameManager.Instance.RegisterPlayer(this);
         }
     }
-    void OnIndexChanged(int oldIndex, int newIndex) { }
+    void OnIndexChanged(int oldIndex, int newIndex) {
+        Debug.Log("변경 전 : " + playerRespawn.playerNumber);
+        playerRespawn.playerNumber = playernumber;
+        Debug.Log("변경 후 : " + playerRespawn.playerNumber);
+    }
 
 	//-----------추락
-	private void OnTriggerEnter(Collider other) {
-		if(other.CompareTag("DeadZone")) {
-            OnFall();
-		}
-	}
+	//private void OnTriggerEnter(Collider other) {
+	//	if(other.CompareTag("DeadZone")) {
+ //           OnFall();
+	//	}
+	//}
 
 	public void OnFall() {
         if (isLocalPlayer) {
